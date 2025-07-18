bf5ef0ffafc78195bc4a3626a49aaf19b4ca7f76
aviraj shinde <aviraj.shinde24@mmit.edu.in>
2025-07-18 10:05:54 +0530

    text

---
+++ simp.py
@@ -0,0 +1,8 @@
+# Check if a number is positive or negative
+num = int(input("Enter a number: "))
+
+if num >= 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
